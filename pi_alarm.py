9909d1a115e3d235dbe062fbb4fc6cb2739d3d8e
Greg McCarthy <greg@gjmccarthy.co.uk>
2018-03-29 13:51:36 +0100

    Updated code to use OpenHab2 to MQTT

--- pi_alarm.py
+++ pi_alarm.py
@@ -3,16 +3,16 @@
 # Rasperry Pi Alarm
 #
 # Author : Greg McCarthy
-# Date   : 24/12/2016
+# Date   : 24/03/2018
 # Version 1.2
 #
 #--------------------------------------
 
-#import
 import sys
 import RPi.GPIO as GPIO
 import time
-import urllib2
+#import urllib2
+#import httplib
 import logging
 from subprocess import *
 from time import sleep
@@ -20,30 +20,34 @@ from datetime import datetime
 import datetime as dtm
 from datetime import timedelta
 
-def url_get(url):     
-        try:
-		logger.info(url)                  
-                urllib2.urlopen(url)                
-        except urllib2.HTTPError as err:
-		logger.info("Error - exception")
-                logger.info(err.code)
-	except Exception:
-    		import traceback
-    		logger.error('generic exception: ' + traceback.format_exc())
+import paho.mqtt.client as mqtt
+import time
 
 logger = logging.getLogger("pi-alarm")
 hdlr = logging.FileHandler("/var/log/pi-alarm.log")
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
-logger.addHandler(hdlr) 
+logger.addHandler(hdlr)
 logger.setLevel(logging.INFO)
 
 BEAM  = 25
 PATIO_DOOR = 18
-PIR_LOUNGE   = 23	# Normally 0 - when triggered goes O/C. So need Pi internal Pull up 	
-FRONT_DOOR = 24		# Normally 0 - when triggered goes to 5V
+PIR_LOUNGE   = 23       # Normally 0 - when triggered goes O/C. So need Pi internal Pull up
+FRONT_DOOR = 24         # Normally 0 - when triggered goes to 5V
+
+def onDisconnect(client, userdata, rc):
+        print("disonnected")
+
+def onConnect(self, client, userdata, rc):
+        print("connected")
 
 # Main program block
+
+client = mqtt.Client(client_id="alarmcontroller")
+client.on_connect = onConnect
+client.on_disconnect = onDisconnect
+client.connect("openhab2.home", 1883, 60)
+client.loop_start()
 
 Lounge_PIR_Triggered_State = False
 Lounge_PIR_Normal_State = False
@@ -58,14 +62,13 @@ Beam_Triggered_State = False
 Beam_Normal_State = False
 
 
-  
+
 GPIO.setwarnings(False)
 
 GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
 
-
-GPIO.setup(BEAM, GPIO.IN)  
-GPIO.setup(PATIO_DOOR, GPIO.IN) 
+GPIO.setup(BEAM, GPIO.IN)
+GPIO.setup(PATIO_DOOR, GPIO.IN)
 GPIO.setup(PIR_LOUNGE, GPIO.IN)
 GPIO.setup(FRONT_DOOR, GPIO.IN)
 
@@ -92,192 +95,134 @@ try:
 
   while True:
 
-	# Check if beam triggered
-	if (GPIO.input(BEAM) == True):
-		if (Beam_Triggered_State == False):	#So we only trigger once
-			logger.info("BEAM Trigger")
-			Beam_Triggered_State = True
-			Beam_Normal_State = False
-			url  = "http://controller.home:8080/json.htm?type=command&param=udevice&idx=26&nvalue=4"
-			url_get(url)
-			BEAM_Time1=dtm.datetime.now()
+        # Check if beam triggered
+        if (GPIO.input(BEAM) == True):
+                if (Beam_Triggered_State == False):     #So we only trigger once
+                        logger.info("BEAM Trigger")
+                        Beam_Triggered_State = True
+                        Beam_Normal_State = False
+                        client.publish("garage/beam", "OPEN")
+                        BEAM_Time1=dtm.datetime.now()
 
-	# Check if beam normal
-	if (GPIO.input(BEAM) == False):
+        # Check if beam normal
+        if (GPIO.input(BEAM) == False):
                 if (Beam_Normal_State == False):
                         BEAM_Time2=dtm.datetime.now()
                         if ((BEAM_Time2-BEAM_Time1).seconds > 60):        #If triggered only return to normal if 60 secs passed
-	                        logger.info("BEAM Normal")
-        	                Beam_Normal_State = True
-				Beam_Triggered_State = False
-                	        url  = "http://controller.home:8080/json.htm?type=command&param=udevice&idx=26&nvalue=1"
-                        	url_get(url)
-			
-	#======================================================================================================
-	
-	# Check if door open
-	if (GPIO.input(FRONT_DOOR) == True):
-		if (FD_Open_State == False):
-                        logger.info("FrontDoor Open")
-                        FD_Open_State = True	# So we only send alert once
-			FD_Closed_State = False
-                        url  = "http://controller.home:8080/json.htm?type=command&param=udevice&idx=19&nvalue=4"
-                        url_get(url)
-			FD_Time1=dtm.datetime.now()
+                                logger.info("BEAM Normal")
+                                Beam_Normal_State = True
+                                Beam_Triggered_State = False
+                                client.publish("garage/beam", "CLOSED")
+				
+        #======================================================================================================
 
-	# Check if door closed
-	if (GPIO.input(FRONT_DOOR) == False):
+        # Check if door open
+        if (GPIO.input(FRONT_DOOR) == True):
+                if (FD_Open_State == False):
+                        logger.info("FrontDoor Open")
+                        FD_Open_State = True    # So we only send alert once
+                        FD_Closed_State = False
+                        client.publish("lounge/frontdoor", "OPEN")
+                        FD_Time1=dtm.datetime.now()
+
+        # Check if door closed
+        if (GPIO.input(FRONT_DOOR) == False):
                 if (FD_Closed_State == False):
                         FD_Time2=dtm.datetime.now()
                         if ((FD_Time2-FD_Time1).seconds > 60):        #If triggered only return to normal if 60 secs passed
-	                        logger.info("FrontDoor Closed")
-        	                FD_Closed_State = True
-				FD_Open_State = False
-                	        url  = "http://controller.home:8080/json.htm?type=command&param=udevice&idx=19&nvalue=1"
-                        	url_get(url)
-        
-	#======================================================================================================
-	
-	#Check if door open
-	if (GPIO.input(PATIO_DOOR) == True):
+                                logger.info("FrontDoor Closed")
+                                FD_Closed_State = True
+                                FD_Open_State = False
+                                client.publish("lounge/frontdoor", "CLOSED")
+
+        #======================================================================================================
+
+        #Check if door open
+        if (GPIO.input(PATIO_DOOR) == True):
                 if (Patio_Door_Open_State == False):
                         logger.info("PatioDoor Open")
                         Patio_Door_Open_State = True
-			Patio_Door_Closed_State = False
-                        url  = "http://controller.home:8080/json.htm?type=command&param=udevice&idx=20&nvalue=4"
-                        url_get(url)
-			PATIO_Time1=dtm.datetime.now()
+                        Patio_Door_Closed_State = False
+                        client.publish("lounge/door", "OPEN")
+                        PATIO_Time1=dtm.datetime.now()
 
-	#Check if door closed
-	if (GPIO.input(PATIO_DOOR) == False):
-		if (Patio_Door_Closed_State == False):
+        #Check if door closed
+        if (GPIO.input(PATIO_DOOR) == False):
+                if (Patio_Door_Closed_State == False):
                         PATIO_Time2=dtm.datetime.now()
                         if ((PATIO_Time2-PATIO_Time1).seconds > 60):        #If triggered only return to normal if 60 secs passed
- 				logger.info("PatioDoor Closed")
-                	        Patio_Door_Closed_State = True
-				Patio_Door_Open_State = False
-                        	url  = "http://controller.home:8080/json.htm?type=command&param=udevice&idx=20&nvalue=1"
-                        	url_get(url)
+                                logger.info("PatioDoor Closed")
+                                Patio_Door_Closed_State = True
+                                Patio_Door_Open_State = False
+                                client.publish("lounge/door", "CLOSED")
 
-	#======================================================================================================
-		
-	#Check if PIR Triggered
-	if (GPIO.input(PIR_LOUNGE) == True):
+        #======================================================================================================
+
+        #Check if PIR Triggered
+        if (GPIO.input(PIR_LOUNGE) == True):
                 if (Lounge_PIR_Triggered_State == False):
                         logger.info("Lounge PIR Trigger")
                         Lounge_PIR_Triggered_State = True
-			Lounge_PIR_Normal_State = False
-                        url  = "http://controller.home:8080/json.htm?type=command&param=udevice&idx=25&nvalue=4"
-       			url_get(url)
-			PIR_Time1=dtm.datetime.now()
+                        Lounge_PIR_Normal_State = False
+                        client.publish("lounge/pir", "OPEN")
 
-	#Check if PIR Normal
+        #Check if PIR Normal
         if (GPIO.input(PIR_LOUNGE) == False):
                 if (Lounge_PIR_Normal_State == False):
-			PIR_Time2=dtm.datetime.now()
-			if ((PIR_Time2-PIR_Time1).seconds > 60):	#If triggered only return to normal if 60 secs passed
-	                        logger.info("Lounge PIR Normal")
-        	                Lounge_PIR_Normal_State = True
-                		Lounge_PIR_Triggered_State = False
-			        url  = "http://controller.home:8080/json.htm?type=command&param=udevice&idx=25&nvalue=1"
-                        	url_get(url)
+                        PIR_Time2=dtm.datetime.now()
+                        if ((PIR_Time2-PIR_Time1).seconds > 60):        #If triggered only return to normal if 60 secs passed
+                                logger.info("Lounge PIR Normal")
+                                Lounge_PIR_Normal_State = True
+                                Lounge_PIR_Triggered_State = False
+                                client.publish("lounge/pir", "CLOSED")
 
 
+        #======================================================================================================
+#
+#       #Every 5 mins send a health check
+#       #Read output from sensors and send to Domoticz
+        if datetime.now() > (dt + timedelta(0,300)):
+                logger.info("Updating Health")
+                dt = datetime.now()
 
-	#======================================================================================================
-#	
-#	#Every 5 mins send a health check
-#	#Read output from sensors and send to Domoticz
-#	if datetime.now() > (dt + timedelta(0,300)):
-#		logger.info("Updating Health")
-#	        dt = datetime.now()
-#
-#		GPIO_Val = GPIO.input(FRONT_DOOR)
-#
-#		if (GPIO_Val ==0):		#If Val is low then value for Domoticz=1, otherwise = 4 (Alarm)
-#			GPIO_Val = 1
-#		else:
-#			GPIO_Val = 4
-#
-#		url = "http://controller.home:8080/json.htm?type=command&param=udevice&idx=19&nvalue={}".format( GPIO_Val )
-#       	url_get(url)
-#
-#		GPIO_Val = GPIO.input(PATIO_DOOR)
-#  
-#               if (GPIO_Val ==0):              #If Val is low then value for Domoticz=1, otherwise = 4 (Alarm)
-#                        GPIO_Val = 1
-#                else:
-#                        GPIO_Val = 4
-#
-#                url = "http://controller.home:8080/json.htm?type=command&param=udevice&idx=20&nvalue={}".format( GPIO_Val )
-#                url_get(url)
-#
-#	 	GPIO_Val = GPIO.input(PIR_LOUNGE)
-#   
-#                if (GPIO_Val ==0):              #If Val is low then value for Domoticz=1, otherwise = 4 (Alarm)
-#                        GPIO_Val = 1
-#                else:
-#                        GPIO_Val = 4     
-#
-#                url = "http://controller.home:8080/json.htm?type=command&param=udevice&idx=25&nvalue={}".format( GPIO_Val )
-#                url_get(url)
-#
-# 		GPIO_Val = GPIO.input(BEAM)
-#
-#                if (GPIO_Val ==0):              #If Val is low then value for Domoticz=1, otherwise = 4 (Alarm)
-#                        GPIO_Val = 1
-#                else:
-#                        GPIO_Val = 4     
-#
-#		url = "http://controller.home:8080/json.htm?type=command&param=udevice&idx=26&nvalue={}".format( GPIO_Val )
-#                url_get(url)
-#		logger.info("Finished Updating Health")
+                GPIO_Val = GPIO.input(FRONT_DOOR)
 
-	sleep(0.1)
+                if (GPIO_Val ==0):              #If Val is low then value for Domoticz=1, otherwise = 4 (Alarm)
+                        client.publish("lounge/frontdoor", "CLOSED")
+                else:
+                        client.publish("lounge/frontdoor", "OPEN")
+
+                GPIO_Val = GPIO.input(PATIO_DOOR)
+
+                if (GPIO_Val ==0):              #If Val is low then value for Domoticz=1, otherwise = 4 (Alarm)
+                        client.publish("lounge/door", "CLOSED")
+                else:
+                        client.publish("lounge/door", "OPEN")
+
+
+                GPIO_Val = GPIO.input(PIR_LOUNGE)
+
+                if (GPIO_Val ==0):              #If Val is low then value for Domoticz=1, otherwise = 4 (Alarm)
+                        client.publish("lounge/pir", "CLOSED")
+                else:
+                        client.publish("lounge/pir", "OPEN")
+
+                GPIO_Val = GPIO.input(BEAM)
+
+                if (GPIO_Val ==0):              #If Val is low then value for Domoticz=1, otherwise = 4 (Alarm)
+                        client.publish("garage/beam", "CLOSED")
+                else:
+                        client.publish("garage/beam", "OPEN")
+
+                logger.info("Finished Updating Health")
+
+        sleep(0.1)
 
 except Exception as e:
-    	logging.exception("message")
+        logging.exception("message")
 
 #finally:
-	GPIO.cleanup()
-	logger.info("Exiting") 
+        GPIO.cleanup()
+        logger.info("Exiting")
 
-
-def switch_triggered(MySwitch):
-	if (MySwitch & 0b00000001) != 0:
-		Alarm = "Door 1"
-		urllib2.urlopen("http://controller.home:8080/json.htm?type=command&param=udevice&idx=19&nvalue=4").read()       #Door 1
-		DOOR1 = True
-	if (MySwitch & 0b00000010) != 0:
-                Alarm = Alarm + "Door 2"
-		urllib2.urlopen("http://controller.home:8080/json.htm?type=command&param=udevice&idx=20&nvalue=4").read()       #Door 2
-		DOOR2 = True
-	if (MySwitch & 0b00000100) != 0:
-                Alarm = Alarm + "Door 3"
-		urllib2.urlopen("http://controller.home:8080/json.htm?type=command&param=udevice&idx=21&nvalue=4").read()       #Door 3
-		DOOR3= True
-	if (MySwitch & 0b00001000) != 0:
-                Alarm = Alarm + "Garage Door 1"
-                urllib2.urlopen("http://controller.home:8080/json.htm?type=command&param=udevice&idx=22&nvalue=4").read()       #Garage Door 1 
-		GARAGE_DOOR1 = True
-       	if (MySwitch & 0b00010000) != 0:
-                Alarm = Alarm + "Garage Door 2"
-                urllib2.urlopen("http://controller.home:8080/json.htm?type=command&param=udevice&idx=23&nvalue=4").read()       #Garage Door 2  
-		GARAGE_DOOR2 = True
-       	if (MySwitch & 0b00100000) != 0:
-                Alarm = Alarm + "Garage Door 3"
-                urllib2.urlopen("http://controller.home:8080/json.htm?type=command&param=udevice&idx=24&nvalue=4").read()       #Garage Door 3  
-		GARAGE_DOOR3 = True
-       	if (MySwitch & 0b01000000) != 0:
-                Alarm = Alarm + "PIR 1"
-                urllib2.urlopen("http://controller.home:8080/json.htm?type=command&param=udevice&idx=25&nvalue=4").read()       #PIR 1  
-		PIR1 = True
-       	if (MySwitch & 0b10000000) != 0:
-                Alarm = Alarm + "PIR 1"
-                urllib2.urlopen("http://controller.home:8080/json.htm?type=command&param=udevice&idx=26&nvalue=4").read()       #PIR 2  
-		PIR2 = True
-
-
-
-
-
+   
